697f37c4b3b61fffad7224b10034097a5652926a
Matt Kirby <MattJKirby@outlook.com>
2022-05-26 23:40:37 +0200

    better mqconsumer

--- home-service/src/messageConnector/mqConsumer.ts
+++ home-service/src/messageConnector/mqConsumer.ts
@@ -1,44 +1,42 @@
-import { Channel } from "amqplib";
 import { mqMessage } from "./mqMessage"
-import { mqChannel } from "./mqChannel";
-import { mqConnection } from "./mqConnection";
+import { mqChannelProvider as mqChannelProvider } from "./mqChannelProvider";
 import { mqHandler } from "./mqHandler";
+import { mqConnection } from "./mqConnection";
 
-export class mqConsumer extends mqChannel {
-   private activeChannel: Promise<Channel>
-   private registeredHandlers: Array<mqHandler> = [];
+export class mqConsumer extends mqChannelProvider {
+    private registeredHandlers: Array<mqHandler> = [];
 
     constructor(connection: mqConnection){
         super(connection);
-        this.activeChannel = super.openNewChannel();
     }
 
     public subscribe = async (queue: string) => {
-        const channel = await this.activeChannel;
-        channel.consume(queue, (msg) => {
-            try{
-                this.executeHandler(JSON.parse(msg!.content.toString()));
-                channel.ack(msg!)
-            } catch (err){
-                channel.nack(msg!);
-            }
-        })
+        const channel = await this.openChannel();
+        if(channel !== undefined){
+            channel.consume(queue, (msg) => {
+                try{
+                    const messageContent = JSON.parse(msg!.content.toString());
+                    this.executeHandler(messageContent, queue);
+                    channel.ack(msg!)
+                } catch (err){
+                    channel.nack(msg!);
+                }
+            })
+        }
     }
 
-    private executeHandler(message: mqMessage){
+    private executeHandler(message: mqMessage, queue: string){
         try {
-            const messageHandler = this.registeredHandlers.find(x => x.handlerToken === message.handlerToken);
+            const messageHandler = this.registeredHandlers.find(x => x.handlerToken === message.handlerToken && x.handlerQueue === queue);
             messageHandler?.invokeHandler(message)
         } catch {
             throw("Handler token not found on received message.")
         }
     }
 
-    public registerMessageHandler = (token: string, handlerFunction: Function, queueName: string): void => {
+    public registerMessageHandler = (queueName: string, token: string, handlerFunction: Function): void => {
         if(this.registeredHandlers.filter(handler => handler.handlerToken === token && handler.handlerQueue === queueName).length < 1){
             this.registeredHandlers.push(new mqHandler(queueName, token, handlerFunction));
         }    
     }
-
-
 }
