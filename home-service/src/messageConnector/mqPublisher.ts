af58c9ce4efa7eb6bde3cfca5959cc6cc276aced
Matt Kirby <MattJKirby@outlook.com>
2022-05-26 23:41:19 +0200

    new publisher

--- home-service/src/messageConnector/mqPublisher.ts
+++ home-service/src/messageConnector/mqPublisher.ts
@@ -1,14 +1,31 @@
 import { Channel } from "amqplib";
-import { mqChannel } from "./mqChannel";
+import { mqChannelProvider } from "./mqChannelProvider";
 import { mqConnection } from "./mqConnection";
 
-export class mqPublisher extends mqChannel{
+export class mqPublisher extends mqChannelProvider {
+    channel?: Promise<Channel>
 
-    constructor(connection: mqConnection){
-        super(connection,{openChannel: true});
+    constructor(connection: mqConnection, options?: {persistingChannel?: boolean}){
+        super(connection);
+        if(options?.persistingChannel == true){
+            this.channel = this.openChannel();
+        }
     }
 
-    public sendMessage = async (queueName: string,message: any):Promise<void> => {
-        (await super.channel!).sendToQueue(queueName, Buffer.from(JSON.stringify(message)))
+    public sendMessage = async (queueName: string,message: any, options?: {usePersistingChannel?: boolean}):Promise<void> => {
+        let channel = this.channel;
+        
+        if(!options?.usePersistingChannel){
+            channel = this.openChannel();
+        }
+
+        const outboundMessage = Buffer.from(JSON.stringify(message));
+
+        channel?.then(ch => {
+            ch.sendToQueue(queueName, outboundMessage);
+            if(!options?.usePersistingChannel){
+                mqChannelProvider.closeActiveChannel(ch);
+            }
+        });
     }
 }
