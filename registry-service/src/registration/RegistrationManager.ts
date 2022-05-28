537f096b5ce2a83a3361dbfe69a44c95f369132e
Matt Kirby <MattJKirby@outlook.com>
2022-05-29 01:04:30 +0200

    Now using new broker implementations

--- registry-service/src/registration/RegistrationManager.ts
+++ registry-service/src/registration/RegistrationManager.ts
@@ -1,30 +1,26 @@
 import { ServiceDbRequests } from "../db/ServiceDbRequests";
-import { BrokerMessage } from "../messageBroker/BrokerMessage";
-import { IBrokerMessage } from "../messageBroker/IBrokerMessage";
-import MessageBroker from "../messageBroker/MessageBroker";
-import { MessageConsumer } from "../messageBroker/MessageConsumer";
-import { MessagePublisher } from "../messageBroker/MessagePublisher";
 import { ServiceStatus } from "../service/ServiceStatus";
 import ServiceSchema from "../../models/ServiceSchema";
 import ServiceRegistrationFactory from "./ServiceRegistrationFactory";
 import { ServiceRegistration } from "../service/ServiceRegistration";
-import { IDatabaseConnection } from "../utility/IDatabaseConnection";
-import mongoose from "mongoose";
+
+import { mqConsumer } from "../messageConnector/mqConsumer";
+import mqConnection from "../messageConnector/mqConnection";
+import { mqPublisher } from "../messageConnector/mqPublisher";
+import { mqMessage } from "../messageConnector/mqMessage";
+import { mqChannelProvider } from "../messageConnector/mqChannelProvider";
 
 /**
  * Service Manager 
  * Controls the registration, handles updates, renewals and deletion of services.
  */
 export class RegistrationManager {
-    readonly queueStub: string = ".registration";
-    registrationConsumer: MessageConsumer
-    registrationPublisher: MessagePublisher
+    readonly registrationQueue: string = "registry-service.registration";
+    registrationConsumer: mqConsumer = new mqConsumer(mqConnection, {persistingChannel: true})
+    registrationPublisher: mqPublisher = new mqPublisher(mqConnection, {persistingChannel: true})
 
     constructor(){
-        this.registrationConsumer = new MessageConsumer(MessageBroker, "registry-service.registration");
-        this.registrationConsumer.subscribe();
-
-        this.registrationPublisher = new MessagePublisher(MessageBroker);
+        this.registrationConsumer.subscribe(this.registrationQueue,{usePersistingChannel: true});
         this.initaliseRegister().then(() => {
             this.bindMessageHandlers(); 
         }) 
@@ -34,19 +30,19 @@ export class RegistrationManager {
      * Registers any relevant message broker handlers 
      */
     private bindMessageHandlers(): void {
-        this.registrationConsumer.registerMessageHandler("register", (msg: IBrokerMessage) => this.handleRegistrationRequest(msg, this.registrationPublisher));
+        this.registrationConsumer.registerMessageHandler(this.registrationQueue,"register", (msg: mqMessage) => this.handleRegistrationRequest(msg, this.registrationPublisher));
     }
 
     /**
      * MessageBroker handler function used for registering new services
      */
-    private handleRegistrationRequest = async (msg: IBrokerMessage, publisher:MessagePublisher) => {
+    private handleRegistrationRequest = async (msg: mqMessage, publisher:mqPublisher) => {
         this.handleUnresponsiveServices();
         if (await ServiceDbRequests.getServiceInstance({"instances.uid": msg.messageContent.uid}) !== null){
             this.handleHealthCheck(msg);
         } else {
-            const registration = await this.newRegistration(msg,publisher);
-            publisher.sendMessage(`${registration.name}.registration`, new BrokerMessage("assignToken", {registrationToken: registration.instance.uid}));
+            const registration = await this.newRegistration(msg);
+            publisher.sendMessage(`${registration.name}.registration`, new mqMessage("assignToken", {registrationToken: registration.instance.uid}));
         }    
     }
 
@@ -54,7 +50,7 @@ export class RegistrationManager {
      * Register new service, assert queue for message broker and send response to generic service queue.
      * If supplied uid is undefined, generate a new uid.
      */
-    private newRegistration = async (msg: IBrokerMessage, publisher:MessagePublisher):Promise<ServiceRegistration> => {
+    private newRegistration = async (msg: mqMessage):Promise<ServiceRegistration> => {
         const registration = ServiceRegistrationFactory.newRegistration(msg.messageContent.metaData, msg.messageContent.uid);
         console.log(`Registering: ${registration.instance.uid}`)
         await ServiceSchema.findOneAndUpdate({name: registration.name, version: registration.version}, { $addToSet: { instances: registration.instance }}, {upsert: true});
@@ -64,7 +60,7 @@ export class RegistrationManager {
     /**
      * Refresh the service healthcheck.
      */
-    private handleHealthCheck = async (msg: IBrokerMessage) => {
+    private handleHealthCheck = async (msg: mqMessage) => {
         await ServiceDbRequests.updateStatusAndHealthCheckByUid(msg.messageContent.uid, msg.messageContent.status)
     }
 
@@ -79,13 +75,14 @@ export class RegistrationManager {
      * Flush all registration records and any pending registration queue messages.
      */
     private initaliseRegister = async () => {
+        const channelProvider = new mqChannelProvider(mqConnection, {persistingChannel: true})
         await ServiceDbRequests.purgeServices();
-        (await this.registrationConsumer.activeChannel).purgeQueue(this.registrationConsumer.queueName);
+       await (await this.registrationConsumer.activeChannel)?.purgeQueue(this.registrationQueue);
        
     }
 
     public test = async (uid: string) => {
         console.log(true,uid)
-        this.registrationPublisher.sendMessage(`${uid}`, new BrokerMessage("test", {test: uid}))  
+        this.registrationPublisher.sendMessage(`${uid}`, new mqMessage("test", {test: uid}))  
     }
 }
