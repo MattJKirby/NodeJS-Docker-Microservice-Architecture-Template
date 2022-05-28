bbfe36e51fbee42cee20e29077f3fe1a0a699c7b
Matt Kirby <MattJKirby@outlook.com>
2022-05-29 01:03:45 +0200

    Copied new env config

---
+++ registry-service/configuration/ConfigurationTypes.ts
@@ -0,0 +1,38 @@
+export interface IConfig {
+    readonly dev: IEnvironmentConfig
+    readonly test: IEnvironmentConfig
+}
+
+export interface IEnvironmentConfig {
+    readonly app: IAppConfig
+    readonly db: IDbConfig
+    readonly mqSettings: IMqConfig
+    readonly serviceRegistration: IRegistrationConfig
+}
+
+export interface IAppConfig {
+    readonly port: number,
+    readonly hostname: string,
+    readonly version: string,
+    readonly name: string,
+    readonly title: string
+}
+
+export interface IDbConfig {
+    readonly host: string,
+    readonly port: number,
+    readonly name: string
+}
+
+export interface IMqConfig {
+    readonly protocol: string,
+    readonly hostname: string,
+    readonly port: number,
+    readonly username: string,
+    readonly password: string
+}
+
+export interface IRegistrationConfig {
+    readonly serviceRequestQueue: string
+    readonly serviceResponseQueue: string
+}
